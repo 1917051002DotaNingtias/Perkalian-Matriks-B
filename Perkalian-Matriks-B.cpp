84fcda45e093aa38b52d3fe2655b6bfe74c4d724
1917051002DotaNingtias <62343494+1917051002DotaNingtias@users.noreply.github.com>
2020-03-19 15:58:06 +0700

    Update Perkalian-Matriks-B.cpp

--- Perkalian-Matriks-B.cpp
+++ Perkalian-Matriks-B.cpp
@@ -4,3 +4,4 @@ using namespace std;
   int main(){
     int matriks1[100][100], matriks2[100][100], hasil[100][100];
     int i, j, k, m, n, p, q, jumlah = 0; 
+    cout << "Masukkan jumlah baris matriks pertama:"; 
