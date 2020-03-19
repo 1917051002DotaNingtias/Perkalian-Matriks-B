9c05e3644562767c660ce67a75a1e88c5fc17b18
Falahyan <62371109+Falahyan@users.noreply.github.com>
2020-03-19 16:03:39 +0700

    Update Perkalian-Matriks-B.cpp

--- Perkalian-Matriks-B.cpp
+++ Perkalian-Matriks-B.cpp
@@ -2,5 +2,13 @@
 using namespace std;
   
   int main(){
-    int matriks1[100][100], matriks2[100][100], hasil[100][100];
-   
+  int matriks1[100][100], matriks2[100][100], hasil[100][100];
+  int i, j, k, a, b, c, d, jumlah = 0;
+  cout << "Masukkan jumlah baris matriks pertama: ";
+  cin >> a;
+  cout << "Masukkan jumlah kolom matriks pertama: ";
+  cin >> b;
+  cout << "Masukkan jumlah baris matriks kedua: ";
+  cin >> c;
+  cout << "Masukkan jumlah kolom matriks kedua: ";
+  cin >> d;
